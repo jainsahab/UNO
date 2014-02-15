09e26243c0552dc759dc6180c43de30260f8845d
soumyagh <soumyagh@thoughtworks.com>
2014-02-15 16:24:37 +0530

    Pick Colour Done (need to fix button type)

--- src/com/tw/uno/view/ChangeColour.java
+++ src/com/tw/uno/view/ChangeColour.java
@@ -22,23 +22,27 @@ public class ChangeColour {
         Border border = BorderFactory.createTitledBorder("Pick Colour");
         panel.setBorder(border);
         ButtonGroup group = new ButtonGroup();
-        AbstractButton abstract1 = new JToggleButton("SET");
-        panel.add(abstract1);
-        group.add(abstract1);
-        AbstractButton abstract2 = new JRadioButton("BLUE");
+        AbstractButton abstract2 = new JCheckBox("BLUE");
         panel.add(abstract2);
         group.add(abstract2);
         AbstractButton abstract3 = new JCheckBox("GREEN");
         panel.add(abstract3);
         group.add(abstract3);
-        AbstractButton abstract4 = new JRadioButtonMenuItem("YELLOW");
+        AbstractButton abstract4 = new JCheckBox("YELLOW");
         panel.add(abstract4);
         group.add(abstract4);
-        AbstractButton abstract5 = new JCheckBoxMenuItem("RED");
+        AbstractButton abstract5 = new JCheckBox("RED");
         panel.add(abstract5);
         group.add(abstract5);
+        AbstractButton abstractButton = new JToggleButton("SET COLOUR");
+        panel.add(abstractButton);
+        group.add(abstractButton);
         frame.add(panel, BorderLayout.CENTER);
         frame.setSize(300, 200);
         frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new ChangeColour();
     }
 }
