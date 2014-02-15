11c907e47cfcde9f9c83a2dd5a3cdcb032cfa28b
Digvijay <digvijag@thoughtworks.com>
2014-02-15 16:19:16 +0530

    Deck of screen completed.

---
+++ src/com/tw/uno/view/OpenedPile.java
@@ -0,0 +1,13 @@
+package com.tw.uno.view;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class OpenedPile extends JButton {
+    public OpenedPile() {
+        setPreferredSize(new Dimension(150, 200));
+        setBackground(Color.green);
+        setVisible(true);
+
+    }
+}
