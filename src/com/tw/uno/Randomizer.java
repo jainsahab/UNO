302c8cee2363b63b7332054ade34c4ddab07ef4a
Prateek <Prateekj@thoughtworks.com>
2014-02-17 11:50:06 +0530

    Algorithm for shuffling completed

---
+++ src/com/tw/uno/Randomizer.java
@@ -0,0 +1,27 @@
+package com.tw.uno;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Randomizer {
+    List list;
+
+    public Randomizer(List list) {
+        this.list = list;
+    }
+
+    public List shuffle(){
+        List<Object> result = new ArrayList();
+        int limit = this.list.size()-1;
+        int count = limit+1;
+        for(int i = 0 ; i < count ; i++){
+            int retrieveNumber = (int) (Math.random()*limit);
+            result.add(this.list.get(retrieveNumber));
+            this.list.remove(retrieveNumber);
+            limit--;
+
+        }
+        return result;
+    }
+
+}
