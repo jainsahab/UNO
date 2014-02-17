f52041631ac78a475e4004249663b394b35a875e
Prateek <Prateekj@thoughtworks.com>
2014-02-17 22:33:25 +0530

    Randomizer file updated

--- test/com/tw/uno/RandomizerTest.java
+++ test/com/tw/uno/RandomizerTest.java
@@ -9,7 +9,7 @@ import java.util.List;
 public class RandomizerTest {
     @Test
     public void testShouldShuffleTheListOfIntegers(){
-        List<Integer> temp = new ArrayList();
+        List<Integer> temp = new ArrayList<>();
         temp.add(1);
         temp.add(2);
         temp.add(3);
@@ -22,7 +22,7 @@ public class RandomizerTest {
 
     @Test
     public void testShouldShuffleTheListOfDoubles(){
-        List<Double> temp = new ArrayList();
+        List<Double> temp = new ArrayList<>();
         temp.add(1.45);
         temp.add(2.67);
         temp.add(3.89);
