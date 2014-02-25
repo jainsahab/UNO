d144a2fb2f2d86db054fb62fd9aa5fcff5ea35b9
Pallavi <pallavig@thoughtworks.com>
2014-02-25 11:53:21 +0530

    #116. shell file and bat file updated.

--- src/run-server.sh
+++ src/run-server.sh
@@ -1,6 +1,1 @@
-echo  "Enter Number Of Players"
-read noOfPlayers
-echo  "Enter Number Of Packs"
-read noOfCards
-echo "You entered $noOfPlayers and $noOfCards"
-java -cp "../lib/*" com.step.programs.ServerApp $noOfPlayers $noOfCards
+java -cp "../lib/*" com.step.programs.ServerApp
